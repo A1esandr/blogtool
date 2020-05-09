01e67f9c8ca3a3c73cf0eede4b58cc0f11ee5bf3
A1esandr <ravulmirt@gmail.com>
2020-05-09 23:28:32 +0500

    Added App

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/net/html"
-	"io/ioutil"
+	app2 "blogtool/src/app"
 	"log"
-	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -15,30 +11,8 @@ func main() {
 	if len(url) == 0 {
 		log.Fatalf("URL not set!")
 	}
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 
-	doc, err := html.Parse(strings.NewReader(string(body)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					fmt.Println(a.Val)
-					break
-				}
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
+	app := app2.NewApp(url)
+	app.Parse()
+	app.Print()
 }
---
+++ src/app/app.go
@@ -0,0 +1,97 @@
+package app
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type App interface {
+	Parse()
+	Print()
+}
+
+type app struct {
+	url      string
+	links    []string
+	rawLinks []string
+	loaded   []string
+}
+
+func NewApp(url string) App {
+	return &app{
+		url: url,
+	}
+}
+
+func (a *app) Parse() {
+	body, err := a.load(a.url)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	doc, err := html.Parse(strings.NewReader(string(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.parse(doc)
+}
+
+func (a *app) Print() {
+	for _, link := range a.rawLinks {
+		fmt.Println(link)
+	}
+}
+
+func (a *app) load(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		err = resp.Body.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func (a *app) parse(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, at := range n.Attr {
+			if at.Key == "href" {
+				fmt.Println(at.Val)
+				if !a.exist(at.Val) {
+					a.add(at.Val)
+				}
+				break
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		a.parse(c)
+	}
+}
+
+func (a *app) exist(url string) bool {
+	for _, link := range a.rawLinks {
+		if url == link {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *app) add(url string) {
+	a.rawLinks = append(a.rawLinks, url)
+}
